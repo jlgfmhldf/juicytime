91642216369edb3e38bb17fc2ce79442d8c1119d
Виктория Земцева <jlgfmhldf@gmail.com>
2016-02-29 00:28:32 +0300

    feat: typing text js & styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,40 @@
 import svg4everybody from 'svg4everybody';
+import typish from 'typish';
 import $ from 'jquery';
 
 $(() => {
 	svg4everybody();
+
+	const
+		$typed = $('.js-typed'),
+		hidden = 'hidden',
+		typeSpeed = 80;
+
+	function typeLinks() {
+		const
+			$typed2 = $('.js-typed-2');
+
+		function link(href) {
+			return '<a href=' + href + ' class="link comming-soon__link">';
+		}
+
+		$typed2.removeClass(hidden);
+
+		typish($typed2)
+			.speed(typeSpeed)
+			.type('ВК', link('http://vk.com/steklofusing'))
+			.type('Одноклассники', link('https://ok.ru/juicytime'))
+			.type('Инстаграм', link('https://www.instagram.com/steklo.fusing/'))
+	}
+
+	typish($typed)
+		.speed(typeSpeed)
+		.type('Здесь скоро будет наш ')
+		.type('веб-сайт', '<nobr>')
+		.type(', а пока вы можете узнать о нас \n')
+		.type('в соц-сетях', '<nobr>')
+		.type('\n И полюбоваться на котика :)')
+		.then(function () { typeLinks() });
+
+
 });
